ffaa919f128062ccb6bcbd66d7a650c4048cc9a6
Nhmives <nhmovies9@gmail.com>
2025-02-23 10:07:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,16 @@
-API_ID       = 
-API_HASH     = "a87c1094edd18650e5dfee0f2bc78bda"
-BOT_TOKEN    = "7280713878:AAHOw5TcAjQIAholI1jLQ2V45A44wnohcsU"
-SESSION      = ""
-DATABASE_URI = "mongodb+srv://nhmovies9:nhmovies9@cluster0.lp65c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "new_updet"
+# Don't Remove Credit Tg - @new_updet
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @insightweav
+
+# Never Give DATABASE_URI And BOT_TOKEN Here If Repo Is Forked Or Public Because This May Lead Account Ban.
+
+import os
+from os import environ
+
+API_ID       = int(environ.get("API_ID", "20496814"))
+API_HASH     = environ.get("API_HASH", "a87c1094edd18650e5dfee0f2bc78bda")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7280713878:AAHOw5TcAjQIAholI1jLQ2V45A44wnohcsU")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://nhmovies9:nhmovies9@cluster0.lp65c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002047453577"))
+ADMIN        = int(environ.get("ADMIN", "6276113288"))
+CHANNEL      = environ.get("CHANNEL", "@new_updet")
