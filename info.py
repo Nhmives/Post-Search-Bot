850cd356fe33b3daecb17942ac056dabca014d22
Nhmives <nhmovies9@gmail.com>
2025-02-23 09:54:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_HASH     = "a87c1094edd18650e5dfee0f2bc78bda"
+BOT_TOKEN    = "7280713878:AAHOw5TcAjQIAholI1jLQ2V45A44wnohcsU"
 SESSION      = ""
-DATABASE_URI = ""
+DATABASE_URI = "mongodb+srv://nhmovies9:nhmovies9@cluster0.lp65c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = 
 ADMIN        = 
 CHANNEL      = "new_updet"
